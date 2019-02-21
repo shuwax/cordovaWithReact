633b657dc0978d14e6dadb8a1410bf52b36c70cf
Sebastian <skobry123on@gmail.com>
2019-02-21 08:59:56 +0100

    cordova part 1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      counter: 0
+    }
+  }
+  onclickAdd = () => {
+    const counter = this.state.counter + 1;
+    this.setState({counter: counter})
+  }
+  onclickMin = () => {
+    const counter = this.state.counter - 1;
+    this.setState({counter: counter})
+  }
+
   render() {
+    const divStyle = {
+      margin: '40px',
+      border: '5px solid pink',
+      width: '300px'
+    };
     return (
       <div className="App">
         <header className="App-header">
@@ -11,6 +32,9 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <button onClick={() => this.onclickAdd()} style={divStyle}>+</button>
+          <button onClick={() => this.onclickMin()} style={divStyle}> -</button>
+          <p>{this.state.counter}</p>
           <a
             className="App-link"
             href="https://reactjs.org"
